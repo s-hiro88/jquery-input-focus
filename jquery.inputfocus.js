e6618933caf0bde03222f85f0cd07cb369d8db12
saku <hi_low8066@hotmail.com>
2026-06-25 17:02:16 +0900

    jQuery4.0対応。

--- jquery.inputfocus.js
+++ jquery.inputfocus.js
@@ -78,7 +78,7 @@
 		// 移動先でkeydownが起こらないようにsetTimeoutする。Firefoxのみの問題
 		setTimeout(function () {
 			var target = $target[0];
-			$target.focus();
+			$target.trigger("focus");
 			if (target && typeof target.select === "function" && !$target.is(":button")) {
 				target.select();
 			}
